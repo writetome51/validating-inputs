e538d37ce8b38822082a9c1ef774789c7eca2c2d
Steve Thompson <writetome51@gmail.com>
2020-08-07 14:05:44 -0600

    Added a small test. Committing version 2.0.0

--- dist/tests.js
+++ dist/tests.js
@@ -1,4 +1,20 @@
-import { ValidatingNumberInputService } from './privy/validating-number-input.service.js';
+import {ValidatingTextInputService} from './privy/validating-text-input.service.js';
 
-export class TestInputService extends ValidatingNumberInputService {
+
+export const ObjectToBind = {property: '-'}
+
+
+export class TestInputService extends ValidatingTextInputService {
+
+	constructor() {
+		super();
+		this.data.objectToBind = ObjectToBind;
+		this.data.propertyToBind = 'property';
+		this.data.isValid = () => this.data.objectToBind[this.data.propertyToBind].length > 0;
+	}
 }
+
+
+let input = new TestInputService();
+if (input.isValid()) console.log('test passed')
+else console.log('test FAILED');
